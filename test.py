aac5e15e43129933f3e8c8e3a3e9d26626883080
Joe Lombrozo <joe@djeebus.net>
2016-08-20 12:16:10 -0700

    tiny progress

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import bs4
 import click
 import http.client
 import logging
+import pprint
 import requests
-import requests.cookies
 import time
 
 
@@ -22,11 +22,10 @@ userAgent = "Mozilla/5.0 " \
 
 
 @click.group()
-@click.option('--email', prompt='Amazon email address: ')
-@click.option('--password', prompt='Amazon password: ', hide_input=True)
+@click.option('--email', prompt='Amazon email address')
+@click.option('--password', prompt='Amazon password', hide_input=True)
 @click.pass_context
 def cli(ctx, email, password):
-    jar = requests.cookies.RequestsCookieJar()
     session = requests.session()
     session.headers['User-Agent'] = userAgent
     ctx.obj = {'session': session}
@@ -36,11 +35,18 @@ def cli(ctx, email, password):
 
 def _login(session, email, password):
     login_url = 'https://www.amazon.com/gp/sign-in.html'
+    request_headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Upgrade-Insecure-Requests': '1',
+    }
 
-    form_response = session.get(login_url)
+    form_response = session.get(login_url, headers=request_headers)
+    print('--- received headers ---\n%s' % form_response.headers)
     print('--- received %s cookies ---' % len(form_response.cookies))
     soup = bs4.BeautifulSoup(form_response.text, 'html5lib')
     form = soup.find('form', {'name': 'signIn'})
+    if not form:
+        print("couln't find form: %s" % form_response.text)
     form_action = form['action']
     inputs = form.find_all('input')
     params = {
@@ -51,8 +57,36 @@ def _login(session, email, password):
     params['email'] = email
     params['password'] = password
 
-    auth_response = requests.post(form_action, params)
-    print('--- received %s cookies ---' % len(auth_response.cookies))
+    print('--- sending parameters ---%s')
+    pprint.pprint(params)
+
+    print('--- sending cookies ---')
+    pprint.pprint(session.cookies.get_dict())
+
+    auth_response = session.post(
+        form_action, params,
+        headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Referer': form_response.url,
+            'Origin': 'https://www.amazon.com',
+        }
+    )
+    print('--- received cookies ---')
+    pprint.pprint(session.cookies.get_dict())
+
+    if 'ap_error_page_cookieless_title' in auth_response.text:
+        print('failed to login: cookieliess page')
+        exit(1)
+    elif 'We can not find an account with that email address' in auth_response.text:
+        print('failed to login: invalid email address')
+        exit(1)
+    elif 'Your password is incorrect' in auth_response.text:
+        print('failed login: invalid password')
+        print('html %s' % auth_response.text)
+        exit(1)
+    elif 'session-token' not in auth_response.cookies:
+        print("failed to receive session token")
+        exit(1)
     import pdb; pdb.set_trace()
 
 
